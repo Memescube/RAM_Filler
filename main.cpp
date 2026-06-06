8a31a5cd2505607d35b35421d2eb859b9463340d
Emilio Craff Castillo <uikza@student.kit.edu>
2026-06-06 22:36:36 +0200

    UI inhancements

--- main.cpp
+++ main.cpp
@@ -3,20 +3,23 @@ using namespace std;
 
 int main() {
 
-    cout << "Enter how much RAM you'd like to fill (in MB):" << endl;
+    cout << "Warning: Allocating more RAM than currently available may cause severe system instability." << endl;
+    cout << "To check available RAM, open Task Manager -> Performance -> Memory." << endl;
+    cout << endl;
+    cout << "Enter the amount of RAM to allocate (in MB): ";
 
     long long ram;
     cin >> ram;
     ram = ram*131000;
     long long* buffer = new long long[ram];
 
-    cout << "Filling RAM, this takes some seconds..." << endl;
+    cout << "Allocating RAM, this may take a few seconds..." << endl;
 
     for (long i=0; i<ram; i++) buffer[i] = i;
 
-    cout << endl;
     cout << "Done!" << endl;
-    cout << "Press Enter to free filled RAM..." << endl;
+    cout << endl;
+    cout << "Press Enter to free allocated RAM..." << endl;
     cin.ignore();
     cin.get();
     delete[] buffer;
