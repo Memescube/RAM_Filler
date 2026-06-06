1a20199452c171f5295933f41ff3f14f6ba29fe3
Emilio Craff Castillo <uikza@student.kit.edu>
2026-06-06 22:05:34 +0200

    works

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "Enter how much RAM you'd like to fill (in MB):" << endl;
+
+    long long ram;
+    cin >> ram;
+    ram = ram*131000;
+    long long* buffer = new long long[ram];
+
+    cout << "Filling RAM, this takes some seconds..." << endl;
+
+    for (long i=0; i<ram; i++) buffer[i] = i;
+
+    cout << "Done!" << endl;
+    cout << "Press Enter to free filled RAM..." << endl;
+    cin.ignore();
+    cin.get();
+    delete[] buffer;
+
+    return 0;
+}
